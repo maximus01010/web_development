6da6e03304a000f0b462acf5350ab8cd23b7f0b1
Sasidhar Reddy <sasidharreddy@pop-os.localdomain>
2024-06-10 12:04:44 +0530

    10-6-24

--- my-app/app/components/Addtocart.tsx
+++ my-app/app/components/Addtocart.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React from 'react'
-
+import Link from 'next/link'
 const Addtocart = () => {
   return (
     <div>
-       <button className='btn btn-primary' onClick={()=>console.log('hiii')}>add to cart</button>
+       <button className='btn btn-primary'>add to cart</button>
+       <Link href='/users'>heyyy</Link>
     </div>
   )
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,14 +6,16 @@ export default function Home() {
     <main>
       <h1>hello</h1>
       <Link href='/users'>userspage</Link>
-      <div className="dropdown dropdown-hover">
-  <div tabIndex={0} role="button" className="btn m-1">Hover</div>
-  <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-    <li><a>Item 1</a></li>
-    <li><a>Item 2</a></li>
-  </ul>
-</div>
-      <Productcard/>
+      {/* <div className="dropdown dropdown-hover">
+        <div tabIndex={0} role="button" className="btn m-1">Hover</div>
+        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+          <li><a>Item 1</a></li>
+          <li><a>Item 2</a></li>
+        </ul>
+        <div>
+        </div>
+      </div> */}
+      <Productcard />
     </main>
   )
 }
